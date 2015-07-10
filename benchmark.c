7ac261ff4202250e24181c7f3770a7f8b9bb4ffc
Joseph Kogut <joseph.kogut@gmail.com>
2015-07-10 10:27:24 -0700

    benchmark.c: Split calculation of GOPS/s into rt_to_gops()

--- benchmark.c
+++ benchmark.c
@@ -29,35 +29,49 @@ unsigned timespec_to_nsec(const struct timespec *start,
 	      ((end->tv_sec - start->tv_sec) * 1000000000);
 }
 
+float rt_to_gops(const double rt)
+{
+	uint64_t total_flops = (uint64_t)BUFFER_SIZE * FLOPS_PER_ITERATION;
+
+	return total_flops / (rt * 1000000000.0f);
+}
+
 void print_perf_stats(const double sec_elapsed)
 {
-	float gflops = (((uint64_t)BUFFER_SIZE * FLOPS_PER_ITERATION) / sec_elapsed) / 1000000000.0f;
-
 	printf("%i Cycles, %i FLOP/iteration, %f sec elapsed\n%f GFLOPS\n\n",
 		BUFFER_SIZE,
 		FLOPS_PER_ITERATION,
 		sec_elapsed,
-		gflops);
+		rt_to_gops(sec_elapsed));
+}
+
+void result_fail(int i, float ferror_pct)
+{
+	printf("Verification failed at %i with %f pct deviation\n",
+		i, ferror_pct);
+}
+
+void result_pass(float max_ferror)
+{
+	printf("Result passed verification. Max ferror %f%%\n\n", max_ferror);
 }
 
 void verify_result(float *a, float *b)
 {
+	const float allowable_error = 1;
 	float max_ferror = 0;
 
 	for (unsigned i = 0; i < BUFFER_SIZE; i++) {
 
 		float ferror_pct = 100.0 / a[i] * fabs(b[i] - a[i]);
 
-		if (ferror_pct > 5) {
-			printf("Verification failed at %i with %f pct deviation\n",
-				i, ferror_pct);
-
-			printf("Expected %f, calculated %f\n\n", a[i], b[i]);
+		if (ferror_pct > allowable_error) {
+			result_fail(i, ferror_pct);
 			return;
 
 		} else if (ferror_pct > max_ferror)
 			max_ferror = ferror_pct;
 	}
 
-	printf("Result passed verification. Max ferror %f%%\n\n", max_ferror);
+	result_pass(max_ferror);
 }
